1ad2fb4d59c76c0ed71a248b765d567dd9a53962
Berkay Sezer <gfberkay251997@hotmail.com>
2020-02-17 21:26:08 +0300

    Class ve class verileri girildi

---
+++ KafeKod.Data/KafeVeri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+    public class KafeVeri
+    {
+        public string Urunler { get; set; }
+        public string AktifSiparisler { get; set; }
+        public string GecmişSiparisler { get; set; }
+    }
+}
---
+++ KafeKod.Data/Siparis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKod.Data
+{
+    public enum SiparisDurum
+    {
+        Aktif, Odendi, Iplal
+    }
+
+    public class Siparis
+    {
+        public int MasaNo { get; set; }
+        public DateTime? AcilisZamani { get; set; }
+        public DateTime? KapanisZamani { get; set; }
+        public SiparisDurum Durum { get; set; }
+
+        public List<SiparisDetay> SiparisDetaylar { get; set; }
+
+        public string ToplamTutarTl => string.Format("{0:0.00}₺", ToplamTutar());
+
+        //public decimal ToplamTutar()
+        //{
+        //    decimal toplam=0;
+        //    foreach (var item in SiparisDetaylar)
+        //    {
+        //        toplam += item.Tutar();
+        //    }
+
+        //    return toplam;
+        //}
+
+        public decimal ToplamTutar() => SiparisDetaylar.Sum(x => x.Tutar());
+
+    }
+
+
+}
